80d25ae8933b6c37a9df4c17f494854d5f1718bd
Tc4dy <140116264+tc4dy@users.noreply.github.com>
2026-01-17 18:47:40 +0300

    Update sentinel_proxy.py

--- sentinel_proxy.py
+++ sentinel_proxy.py
@@ -22,7 +22,58 @@ class SentinelProxy:
             "https://free-proxy-list.net/",
             "https://www.us-proxy.org/",
             "https://www.socks-proxy.net/",
-            "https://www.sslproxies.org/"
+            "https://www.sslproxies.org/",
+            "https://www.proxy-list.download/HTTP",
+            "https://www.proxy-list.download/HTTPS",
+            "https://www.proxy-list.download/SOCKS4",
+            "https://www.proxy-list.download/SOCKS5",
+            "https://api.proxyscrape.com/v2/?request=get&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all",
+            "https://api.proxyscrape.com/v2/?request=get&protocol=socks4&timeout=10000&country=all",
+            "https://api.proxyscrape.com/v2/?request=get&protocol=socks5&timeout=10000&country=all",
+            "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt",
+            "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks4.txt",
+            "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks5.txt",
+            "https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/http.txt",
+            "https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/https.txt",
+            "https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/socks4.txt",
+            "https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/socks5.txt",
+            "https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/http.txt",
+            "https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/socks4.txt",
+            "https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/socks5.txt",
+            "https://raw.githubusercontent.com/mmpx12/proxy-list/master/http.txt",
+            "https://raw.githubusercontent.com/mmpx12/proxy-list/master/https.txt",
+            "https://raw.githubusercontent.com/mmpx12/proxy-list/master/socks4.txt",
+            "https://raw.githubusercontent.com/mmpx12/proxy-list/master/socks5.txt",
+            "https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt",
+            "https://raw.githubusercontent.com/sunny9577/proxy-scraper/master/proxies.txt",
+            "https://raw.githubusercontent.com/UptimerBot/proxy-list/main/proxies/http.txt",
+            "https://raw.githubusercontent.com/UptimerBot/proxy-list/main/proxies/socks4.txt",
+            "https://raw.githubusercontent.com/UptimerBot/proxy-list/main/proxies/socks5.txt",
+            "https://raw.githubusercontent.com/roosterkid/openproxylist/main/HTTPS_RAW.txt",
+            "https://raw.githubusercontent.com/roosterkid/openproxylist/main/SOCKS4_RAW.txt",
+            "https://raw.githubusercontent.com/roosterkid/openproxylist/main/SOCKS5_RAW.txt",
+            "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-http.txt",
+            "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-https.txt",
+            "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-socks4.txt",
+            "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-socks5.txt",
+            "https://raw.githubusercontent.com/hookzof/socks5_list/master/proxy.txt",
+            "https://raw.githubusercontent.com/rdavydov/proxy-list/main/proxies/http.txt",
+            "https://raw.githubusercontent.com/rdavydov/proxy-list/main/proxies/https.txt",
+            "https://raw.githubusercontent.com/rdavydov/proxy-list/main/proxies/socks4.txt",
+            "https://raw.githubusercontent.com/rdavydov/proxy-list/main/proxies/socks5.txt",
+            "https://raw.githubusercontent.com/zevtyardt/proxy-list/main/http.txt",
+            "https://raw.githubusercontent.com/Zaeem20/FREE_PROXIES_LIST/master/http.txt",
+            "https://raw.githubusercontent.com/Zaeem20/FREE_PROXIES_LIST/master/https.txt",
+            "https://raw.githubusercontent.com/Zaeem20/FREE_PROXIES_LIST/master/socks4.txt",
+            "https://raw.githubusercontent.com/Zaeem20/FREE_PROXIES_LIST/master/socks5.txt",
+            "https://www.proxyscan.io/download?type=http",
+            "https://www.proxyscan.io/download?type=https",
+            "https://www.proxyscan.io/download?type=socks4",
+            "https://www.proxyscan.io/download?type=socks5",
+            "https://proxyspace.pro/http.txt",
+            "https://proxyspace.pro/https.txt",
+            "https://proxyspace.pro/socks4.txt",
+            "https://proxyspace.pro/socks5.txt"
         ]
         
         self.banner = f"""
@@ -53,20 +104,36 @@ class SentinelProxy:
         print(f"{Fore.BLUE}[*] {self.msg('Scraping proxies from multiple sources...', 'Birden fazla kaynaktan proxy toplanıyor...')}")
         
         found_count = 0
+        proxies_set = set()
+        
         for url in self.sources:
             try:
                 response = requests.get(url, timeout=10)
-                soup = BeautifulSoup(response.text, "html.parser")
-                table = soup.find("table")
-                if table:
-                    for row in table.find_all("tr")[1:]:
-                        tds = row.find_all("td")
-                        if len(tds) >= 2:
-                            ip = tds[0].text.strip()
-                            port = tds[1].text.strip()
-                            proxy = f"{ip}:{port}"
-                            self.proxies_to_check.put(proxy)
-                            found_count += 1
+                
+                if "raw.githubusercontent.com" in url or "proxyscrape.com" in url or "proxyscan.io" in url or "proxyspace.pro" in url:
+                    lines = response.text.strip().split('\n')
+                    for line in lines:
+                        line = line.strip()
+                        if ':' in line and line not in proxies_set:
+                            parts = line.split(':')
+                            if len(parts) == 2:
+                                proxies_set.add(line)
+                                self.proxies_to_check.put(line)
+                                found_count += 1
+                else:
+                    soup = BeautifulSoup(response.text, "html.parser")
+                    table = soup.find("table")
+                    if table:
+                        for row in table.find_all("tr")[1:]:
+                            tds = row.find_all("td")
+                            if len(tds) >= 2:
+                                ip = tds[0].text.strip()
+                                port = tds[1].text.strip()
+                                proxy = f"{ip}:{port}"
+                                if proxy not in proxies_set:
+                                    proxies_set.add(proxy)
+                                    self.proxies_to_check.put(proxy)
+                                    found_count += 1
             except Exception as e:
                 continue
         
